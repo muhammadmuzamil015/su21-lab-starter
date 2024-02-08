2f93283707aecf2c9dd7e6c80217e9372a27da23
MuhammadMuzamil015 <bsce19015@itu.edu.pk>
2024-02-08 22:36:03 +0500

    all five exercises of lab03 are completed

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -42,24 +42,24 @@ main:
     la s1, source
     la s2, dest
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
-    sw t0, 0(sp)
-    sw t2, 4(sp)
-    jal fun
-    lw t0, 0(sp)
-    lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
-    jal x0, loop
+    slli s3, t0, 2      # shifting t0 by 2bits (multiplying by 4)
+    add t1, s1, s3      # add shifted number to base address of source and store the result to t1
+    lw t2, 0(t1)        # load the word from 0 offset of source array and store to t2
+    beq t2, x0, exit    # if the word/element is 0 then goto exit label
+    add a0, x0, t2      # if word/element from source is not 0, store it to a0 (argument)
+    addi sp, sp, -8     # booked the stack for 2words(8 bytes)
+    sw t0, 0(sp)        # store t0 to sp by 0 offset 
+    sw t2, 4(sp)        # store t0 to sp by 4 offset
+    jal fun             # jump and link to fun
+    lw t0, 0(sp)        # restore t0 from stack
+    lw t2, 4(sp)        # restore t1 from stack
+    addi sp, sp, 8      # free up stack
+    add t2, x0, a0      # move the function return value which is in a0 to t2
+    add t3, s2, s3      # add the shifted number to base address of dest and store the result to t3
+    sw t2, 0(t3)        # store the value retured by function which is now in t2 to 0 offset of dest array
+    add s0, s0, t2      # add the t0 value returned by function to s0 which is sum
+    addi t0, t0, 1      # increment the t0 which is k
+    jal x0, loop        # jump and link to label loop
 exit:
     add a0, x0, s0
     # BEGIN EPILOGUE
